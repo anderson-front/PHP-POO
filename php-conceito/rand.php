76435972044fcc232d038b60db85e7acda167e03
Anderson Santos <anderson.santos.silva@live.com>
2021-01-01 19:12:32 -0300

    rand: Gera n° aleatório.

---
+++ php-conceito/rand.php
@@ -0,0 +1,14 @@
+<?php
+
+// (PHP 4, PHP 5, PHP 7)
+
+// rand — Gera um inteiro aleatório
+
+// Descrição ¶
+// rand ( ) : int
+// rand ( int $min , int $max ) : int
+
+
+$aleatorio=rand(0,1000);
+echo $aleatorio;
+

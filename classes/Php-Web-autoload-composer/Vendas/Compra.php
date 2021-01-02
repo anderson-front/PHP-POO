543985cf9c57a985a441ba846e7a077c7482948f
Anderson Santos <anderson.santos.silva@live.com>
2021-01-02 17:34:19 -0300

    Exemplo de autoload usando Composer Class Map

---
+++ classes/Php-Web-autoload-composer/Vendas/Compra.php
@@ -0,0 +1,30 @@
+<?php
+
+class Compra
+{
+    public $id, $produtos, $usuario;
+
+    public function cadastrar(array $produtos, Usuario $usuario)
+    {
+
+        $this->id = rand(0, 1000);
+        $this->produtos = $produtos;
+        $this->usuario = $usuario;
+    }
+
+    public function imprimir()
+    {
+        $r = 'Compra Id: ' . $this->id . '<hr>';
+        $r.= 'Produtos: ' . '<br>';
+
+
+        foreach ($this->produtos as $produto) {
+
+            $r.= $produto->imprimir();// Erro 
+        
+        }
+        $r.='<hr>';
+        $r.=$this->usuario->imprimir();
+        echo $r;
+    }
+}
---
+++ classes/Php-Web-autoload-composer/index.php
@@ -0,0 +1,34 @@
+<?php
+//Ao Criar novas Classes executar novamente o comando composer dump-autoload
+include 'vendor/autoload.php';
+$u = new Usuario();
+$u->cadastrar('Anderson Santos', 35);
+
+// $u->imprimir();
+
+$p1 = new Produto();
+$p2 = new Produto();
+$p3 = new Produto();
+$p4 = new Produto();
+$p5 = new Produto();
+
+
+$p1->cadastrar('1', 'Geladeira');
+$p2->cadastrar('2', 'Fogão');
+$p3->cadastrar('3', 'Sofá');
+$p4->cadastrar('4', 'Tv');
+$p5->cadastrar('5', 'Celular');
+
+$c = new Compra();
+
+$c->cadastrar(
+    array(
+        'p1' => $p1,
+        'p2' => $p2,
+        'p3' => $p3,
+        'p4' => $p4,
+        'p5' => $p5
+    ),
+    $u
+);
+$c->imprimir();
